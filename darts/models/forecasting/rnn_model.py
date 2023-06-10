5884c99f8660ef0d46c00999348340f64da5d203
Leonie Freisinger <ge79kej@mytum.de>
2023-06-09 20:59:07 -0700

    add hidden dim

--- darts/models/forecasting/rnn_model.py
+++ darts/models/forecasting/rnn_model.py
@@ -79,7 +79,7 @@ class _RNNModule(PLDualCovariatesModule):
 
         #RevIn
         from darts.models.forecasting.RevIN import RevIN
-        self.revin_layer = RevIN(hidden_dim)
+        self.revin_layer = RevIN(1) # 1 for point forecasts
 
         # Defining the RNN module
         self.rnn = getattr(nn, name)(
